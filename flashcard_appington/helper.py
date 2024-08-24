94a14ac55c666de96084537a879cf5cb8f7a3ab1
lbj1533 <lxbxjx15@outlook.com>
2024-08-24 12:50:24 +0200

    rebase

--- flashcard_appington/helper.py
+++
@@ -1,6 +0,0 @@
-def start_app():
-    """
-    This function is a placeholder for any initialization or startup logic
-    that might be needed for the application.
-    """
-    pass
--- flashcard_appington/main.py
+++ flashcard_appington/main.py
@@ -1,28 +1,16 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flashcard_appington.handlers import MenuHandler, IOHandler, PrintHandler
 from flashcard_appington.models import CardSet, Settings
-from flashcard_appington.helper import start_app
 
 def main():
-    # Initialize settings
-    settings = Settings.default_settings()
-
-    # Show settings menu and modify if needed
-    settings = MenuHandler.display_settings(settings)
-
-    # Choose a file to study
-    file_path = MenuHandler.choose_file("flashcards/Swedish/flashcards")
-
-    # Create a CardSet instance from the chosen file
-    card_set = CardSet(file_path)
-    
-    # Start studying with the cards
-    card_set.study(settings)
-
-    # Repeat study session if needed
+    settings = MenuHandler.display_settings(Settings.default_settings())
+    file_path = MenuHandler.choose_file("flashcards/Swedish")
+    card_set = CardSet(file_path).study(settings)
     MenuHandler.prompt_repeat(card_set.cards, file_path, settings)
-
-    # Display welcome message
-    PrintHandler.print_notice("Starting Flashcard Appington!")
     
 
 if __name__ == "__main__":
